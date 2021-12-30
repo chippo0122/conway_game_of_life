7d4320710511f1371c3c8cb10813ad06fc1fbe2d
chippo0122 <79824220+chippo0122@users.noreply.github.com>
2021-12-30 20:16:47 +0800

    2021.12.30 unfinish update

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -8,7 +8,7 @@ const INITIAL_STATE = {
     cols: 100,
     rows: 50,
     generations: 0,
-    gridFull: [],
+    gridFull: []
 }
 
 const reducer = (state, action) => {
@@ -28,7 +28,7 @@ const reducer = (state, action) => {
             const seedArr = [...state.gridFull];
             for (let i = 0; i < state.rows; i++) {
                 for (let j = 0; j < state.cols; j++) {
-                    const sep = Math.floor(Math.random() * 4);
+                    const sep = Math.floor(Math.random() * 8);
                     if (sep === 1) {
                         seedArr[i][j] = true;
                     }
@@ -36,96 +36,105 @@ const reducer = (state, action) => {
             }
             return { ...state, gridFull: seedArr };
 
-        case 'PLAY': 
-            const {gridFull, rows, cols} = state;
-            let g = gridFull;
+        case 'PLAY':
+            const { gridFull, rows, cols } = state;
+            let g = [...gridFull];
             let gPost = [...gridFull];
-            for(let i = 0; i < rows; i ++) {
-                for(let j = 0 ; j < cols; j ++) {
+            for (let i = 0; i < rows; i++) {
+                for (let j = 0; j < cols; j++) {
                     let count = 0;
-                    if(i - 1 >= 0) {
-                        if(g[i - 1][j]) count ++;
-                    } 
-                    if(i - 1 >= 0 && j - 1 >= 0) {
-                        if(g[i - 1][j - 1]) count ++;
+                    if (i - 1 >= 0) {
+                        if (g[i - 1][j]) count++;
                     }
-                    if(i - 1 >= 0 && j + 1 < cols) {
-                        if(g[i - 1][j + 1]) count ++;
+                    if (i - 1 >= 0 && j - 1 >= 0) {
+                        if (g[i - 1][j - 1]) count++;
                     }
-                    if(j - 1 >= 0) {
-                        if(g[i][j - 1]) count ++;
+                    if (i - 1 >= 0 && j + 1 < cols) {
+                        if (g[i - 1][j + 1]) count++;
                     }
-                    if(j + 1 < cols) {
-                        if(g[i][j + 1]) count ++;
+                    if (j - 1 >= 0) {
+                        if (g[i][j - 1]) count++;
                     }
-                    if(i + 1 < rows) {
-                        if(g[i + 1][j]) count ++;
+                    if (j + 1 < cols) {
+                        if (g[i][j + 1]) count++;
                     }
-                    if(i + 1 < rows && j - 1 >= 0) {
-                        if(g[i + 1][j - 1]) count ++
+                    if (i + 1 < rows) {
+                        if (g[i + 1][j]) count++;
                     }
-                    if(i + 1 < rows && j + 1 < cols) {
-                        if(g[i + 1][j + 1]) count ++
+                    if (i + 1 < rows && j - 1 >= 0) {
+                        if (g[i + 1][j - 1]) count++
                     }
-                    
-                    if(g[i][j]) {
-                        if(count < 2 || count > 3) {
+                    if (i + 1 < rows && j + 1 < cols) {
+                        if (g[i + 1][j + 1]) count++
+                    }
+
+                    if (g[i][j]) {
+                        if (count < 2 || count > 3) {
                             gPost[i][j] = false;
-                        } 
+                        }
                     } else {
-                        if(count === 3) {
+                        if (count === 3) {
                             gPost[i][j] = true;
                         }
                     }
                 }
             }
 
-            return {...state, gridFull: gPost, generations: state.generations ++}
+            return { ...state, gridFull: gPost, generations: state.generations + 1 }
+
         case 'CLEAR':
             const resetArr = new Array(state.rows).fill().map(() => new Array(state.cols).fill(false));
-            return {...INITIAL_STATE, gridFull: resetArr}
+            return { ...INITIAL_STATE, gridFull: resetArr }
+
         default:
             return INITIAL_STATE;
     }
 }
 
-
 export default function Main() {
     const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
-    const [gen, setGen] = useState(0);
-    let timer = null;
+    const [timer, setTimer] = useState(null);// store setInterval
+    const [isActive, setActive] = useState(false);
 
     useEffect(() => {
         dispatch({ type: 'SET_MAP' });
     }, [])
 
     useEffect(() => {
-        console.log('gen is change');
-        setGen(state.generations);
-    }, [state.generations])
+        if(isActive) {
+            const interval = setInterval(() => {
+                dispatch({type: 'PLAY'});
+            }, state.speed)
+            setTimer(interval);
+        } else {
+            clearInterval(timer);
+            setTimer(null);
+        }
+    }, [isActive])
 
     const triggerPlay = () => {
-        window.clearInterval(timer);
-        timer = window.setInterval(() => {
-            dispatch({type: 'PLAY'});
-            console.log('tick');
-        }, state.speed);
+        setActive(true);
     }
 
     const stopPlay = () => {
-        window.clearInterval(timer);
+        setActive(false);
+    }
+
+    const clear = () => {
+        stopPlay();
+        dispatch({ type: 'CLEAR' });
     }
 
     return (
         <div className='main container mx-auto'>
             <button className='btn btn-primary' onClick={triggerPlay}>PLAY</button>
             <button className='btn btn-danger' onClick={stopPlay}>STOP</button>
-            <button className='btn btn-warning' onClick={() => {dispatch({type: 'CLEAR'})}}>CLEAR</button>
+            <button className='btn btn-warning' onClick={clear}>CLEAR</button>
             <button className='btn btn-primary' onClick={() => { dispatch({ type: 'SET_SEED' }) }}>SEED</button>
             <stateContext.Provider value={state}>
                 <Grid dispatch={dispatch} />
             </stateContext.Provider>
-            <h2 className='fs-4'>Generations:{gen}</h2>
+            <h2 className='fs-4'>Generations: {state.generations}</h2>
         </div>
     )
 }
